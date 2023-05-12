72038d92dfbc20837936d12237f5cec03e3fd9d5
Timur010 <duakar010@gmail.com>
2023-05-12 08:08:47 +0300

    Initial Commit

---
+++ DGUNH/DGUNHApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGUNHApp.swift
+//  DGUNH
+//
+//  Created by timur on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DGUNHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
