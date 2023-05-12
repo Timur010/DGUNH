2a4471a3805f362e6d24d983736057ccc00b0bf2
Timur010 <duakar010@gmail.com>
2023-05-13 00:41:43 +0300

    Add icons and colors

---
+++ DGUNH/Extention/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  DGUNH
+//
+//  Created by timur on 13.05.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static var accent: Color {
+        return Color("Accent")
+    }
+    
+    static var background: Color {
+        return Color("Background")
+    }
+    
+    static var secondary: Color {
+        return Color("Secondary")
+    }
+    
+    static var tabBar: Color {
+        return Color("TabBar")
+    }
+}
